7af971a559f1b3f9a0da65fb5801515511bc23e1
Jonathan Dumlao <dumlao.jonathan.g.5165@gmail.com>
2023-11-28 12:27:15 +0800

    fix no module found

---
+++ components/card/CardWrapper.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import DeleteButton from "./DeleteButton";
+import { timeFormat } from "@/libs/timeFormat";
+import { Todo } from "@prisma/client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Badge } from "../ui/badge";
+
+const CardWrapper = ({
+  props,
+  children,
+}: {
+  props: Todo;
+  children: React.ReactNode;
+}) => {
+  return (
+    <Card className="flex flex-col justify-around ">
+      <CardHeader>
+        <CardTitle className="flex items-center justify-between gap-4">
+          <p className="line-clamp-1 w-1/2">{props.title}</p>
+          <Badge className="w-fit">{props.status}</Badge>
+        </CardTitle>
+        <CardDescription className="flex flex-col">
+          <span> Created: {timeFormat(props.createdAt)}</span>
+          <span>
+            {props.status === "DONE" &&
+              `Completed: ${timeFormat(props.updatedAt)}`}
+          </span>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p className="break-words">{props.description}</p>
+      </CardContent>
+      <CardFooter className="flex gap-2">
+        <DeleteButton id={props.id} />
+        {children}
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default CardWrapper;
